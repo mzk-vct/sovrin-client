17303fed9d8e7db0a940a1140a90c41f54a7e208
Victor Muzychenko <mzkvct@gmail.com>
2017-04-17 18:28:26 +0300

    up common version

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
         '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.sovrin']},
     include_package_data=True,
-    install_requires=['sovrin-common-dev==0.2.33', 'anoncreds-dev==0.3.5'],
+    install_requires=['sovrin-common-dev==0.2.35', 'anoncreds-dev==0.3.5'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'sovrin-node-dev==0.3.32'],
     scripts=['scripts/sovrin', 'scripts/change_node_ha',
